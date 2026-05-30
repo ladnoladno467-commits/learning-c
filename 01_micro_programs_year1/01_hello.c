a632ab9ad7d4e7003a741d67ac76a1b2d25c5fe1
pep <ladnoladno467@gmail.com>
2026-05-30 19:59:00 +0400

    progtram and learning26

---
+++ 01_micro_programs_year1/01_hello.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(void) {
+	int a = 1;
+	int b;
+	int cul;
+	printf("what you old yars?\n");
+		scanf("%d", &b);
+		cul = a + b; 
+		printf("your age next year:%d\n",cul);
+	return 0;
+}
